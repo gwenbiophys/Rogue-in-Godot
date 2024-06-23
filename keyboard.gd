5d528d870812e2f582e6a63ee7b44f0146ccb1c5
Gwenevere Kucera <69560038+gwenbiophys@users.noreply.github.com>
2024-06-23 01:42:24 -0400

    k

---
+++ keyboard.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is act-up:
+		# move up. do we have to evaluate 13 + combinations of key presses at each player input?
+
