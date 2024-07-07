9d63faaa52c23ad2c37ed7682adb0b8616fcbe93
Rowan <113938032+DeleteRowan@users.noreply.github.com>
2024-07-07 12:58:45 -0400

    cleaning up code

--- scripts/newLevel.gd
+++ scripts/newLevel.gd
@@ -4,13 +4,8 @@ extends Node
 var level_number: int = 1
 
 
-
-class floor_parameters:
-	const rooms_count: int = 9
-	const x_length: int = 42 # from original rogue, mac may be different
-	const y_length: int = 30 # from original rogue, mac may be different
- 
-class room_parameters:
+class room:
+	# parameters 
 	#const rooms_min: int = 1
 	const rooms_max: int = 9
 	const x_min: int = 2
@@ -19,18 +14,8 @@ class room_parameters:
 	const y_max: int = floor_parameters.y_length / 3
 	const doors_min: int = 1
 	const doors_max: int = 4
-	
 	const isgone_max: int = 4
-	var isdark: bool
-	var ismaze: bool
 	
-	const treasureroom_chance: float = 1/20
-	const treasures_max: int = 10
-	const treasures_min: int = 2
-	const monsters_min: int = 0
-	const monsters_max: int = 10 # fill in value, i have no idea, might not even be a limit!
-
-class room:
 	var isgone: bool
 	var isdark: bool
 	var ismaze: bool
@@ -44,22 +29,41 @@ class room:
 
 
 
-class level:
+class floor:
+	#parameters 
+	const rooms_count: int = 9
+	const x_length: int = 42 # from original rogue, mac may be different
+	const y_length: int = 30 # from original rogue, mac may be different
+	
+	const treasureroom_chance: float = 1/20
+	const treasures_max: int = 10
+	const treasures_min: int = 2
+	const monsters_min: int = 0
+	const monsters_max: int = 10 # fill in value, i have no idea, might not even be a limit!
+
 	#var rooms_count: int
 	var rooms: Array # array of rooms 
 
 
 # Called when the node enters the scene tree for the first time.
-func generate_level(floorP, roomP, room, floor): # what is floorP and roomP? theyre not defined anywhere 
-	var thisFloor = level.new 
+func generate_floor(room, floor): 
+	var thisFloor = floor.new 
 	
-	for i in range(1, floorP.rooms_count):
-		thisFloor.rooms.append(generate_room(floorP, roomP)) 
-		# for a var, cant just call the "level" class, have to create 
-		# an object with the level class and then call that object.rooms 
+	for i in range(1, floor.rooms_count):
+		thisFloor.rooms.append(generate_room(floor, room)) 
+		# for a var, cant just call the "floor" class, have to create 
+		# an object with the floor class and then call that object.rooms 
 
-func generate_room(floorP, roomP):
+func generate_room(floor, room):
+	# decide room type 
 	
+	# decide location of room 
+	
+	# decide size of room 
+	
+	# add a monster 
+	
+	# add gold 
 	return room
 # if the player is below the amulet spawn floor and does not ahve it, then the
 # the game will place it down, seemingly at each level.
