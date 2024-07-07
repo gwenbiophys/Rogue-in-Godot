e2256296b5ecdd068d51b3718e59de96bd2bc901
Rowan <113938032+DeleteRowan@users.noreply.github.com>
2024-07-07 14:45:59 -0400

    more work on level generation

--- scripts/newLevel.gd
+++ scripts/newLevel.gd
@@ -9,15 +9,13 @@ class rfloor: # changed to "rfloor" to avoid conflict with existing godot functi
 	const x_length: int = 42 # from original rogue, mac may be different
 	const y_length: int = 30 # from original rogue, mac may be different
 	
-	const treasureroom_chance: float = 1/20
+	const treasureroom_chance: float = 0.05
 	const treasures_max: int = 10 # in treasure room, specifically 
 	const treasures_min: int = 2 # in treasure room 
 	
 	const monsters_min: int = 0
 	#const monsters_max: int = 10 # fill in value, i have no idea, might not even be a limit!
-	
 	const traps_max: int = 10 
-	
 	#var rooms_count: int
 	var rooms: Array # array of rooms 
 
@@ -25,15 +23,17 @@ class room:
 	# parameters 
 	#const rooms_min: int = 1
 	const rooms_max: int = 9
-	const x_min: int = 2
-	const y_min: int = 2
-	const x_max: int = rfloor.x_length / 3
-	const y_max: int = rfloor.y_length / 3
+	const x_min: int = 2 
+	const y_min: int = 2 
+	const x_max: int = rfloor.x_length / 3 # same as bsze.x in source code (box size) 
+	const y_max: int = rfloor.y_length / 3 # bsze.y
 	const doors_min: int = 1
 	const doors_max: int = 4
 	const isgone_max: int = 4
 	
-	var box: Vector2 # coords of upper left corner of "box" room is in 
+	var box: Vector2 # coords of upper left corner of "box" room is in, same as top.x and top.y in source code
+	var size: Vector2 # size of room 
+	var pos: Vector2 # position of room 
 	
 	var isgone: bool
 	var isdark: bool
@@ -49,22 +49,22 @@ class room:
 
 # Called when the node enters the scene tree for the first time.
 func generate_floor(room, rfloor): 
-	var thisFloor = rfloor.new 
+	var thisFloor = rfloor.new()
 	
-	for i in range(1, rfloor.rooms_count):
-			
-		thisFloor.rooms.append(generate_room(rfloor, room, i)) 
-		# for a var, cant just call the "floor" class, have to create 
-		# an object with the floor class and then call that object.rooms 
+	for i in range(0, rfloor.rooms_count): # 0 - 8, range is exclusive of the second value 
+		thisFloor.rooms = [0]
+		var rooms = thisFloor.rooms
+		rooms.push_back(generate_room(rfloor, room, i)) 
 
 func generate_room(rfloor, room, i):
 	# create a room object 
-	var thisRoom = room.new 
+	var thisRoom = room.new() 
 	
-	# determine which "box" this room is in (top left corner)
-	#thisRoom.box = Vector2.ZERO
-	#thisRoom.box[0] = i % 3 * room.x_max + 1
-	#thisRoom.box[1] = i / 3 * room.y_max 
+	#determine which "box" this room is in (top left corner)
+	#print(i)
+	thisRoom.box = Vector2.ZERO
+	thisRoom.box[0] = i % 3 * room.x_max + 1
+	thisRoom.box[1] = i / 3 * room.y_max 
 	#print(thisRoom.box)
 	
 	# decide room type 
@@ -76,10 +76,20 @@ func generate_room(rfloor, room, i):
 		if(randi_range(0,15) == 0):
 			thisRoom.ismaze = true
 	
-	# decide location of room 
-	
-	
-	# decide size of room 
+	# decide size and location of room 
+	if thisRoom.ismaze == true:
+		pass
+	else: 
+		# choose size of room 
+		while(thisRoom.pos.y <= 0):
+			thisRoom.size.x = randi_range(0, room.x_max - 4) + 4 
+			thisRoom.size.y = randi_range(0, room.y_max - 4) + 4 
+			# choose position of room based on size 
+			thisRoom.pos.x = thisRoom.box.x + randi_range(0, room.x_max - thisRoom.size.x)
+			thisRoom.pos.y = thisRoom.box.y + randi_range(0, room.y_max - thisRoom.size.y)
+			# if outside map, redo 
+			# this only checks for the y position, not sure if we need to also check for the x pos? 
+		#print(thisRoom.pos)
 	
 	# add a monster 
 	
@@ -88,6 +98,10 @@ func generate_room(rfloor, room, i):
 # if the player is below the amulet spawn floor and does not ahve it, then the
 # the game will place it down, seemingly at each level.
 
+func render_room(room): 
+	pass 
+	# renders the tiles for the rooms 
+	# first, lets try just the top corner 
 
 
 # Called when the node enters the scene tree for the first time.
